0b5b01763aef847cadfa2cab67bc4a8da7a029b2
Nagaraju0323 <nagarajukios85@gmail.com>
2024-06-04 22:19:44 +0530

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Nagaraju on 04/06/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ GroceryApp/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  GroceryApp
+//
+//  Created by Nagaraju on 04/06/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
